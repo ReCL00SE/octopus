40a01f707f95523e6a1d589534d4664fbbf10bf4
ReCL00SE <95970740+ReCL00SE@users.noreply.github.com>
2022-10-01 21:59:02 +0500

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,8 @@
+$('.my_fade').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: $('.slick-arrow.left'),
+    nextArrow: $('.slick-arrow.right'),
+  });
