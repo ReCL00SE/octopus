2dc4018421dfc4b03e1c3395280fee776c93db52
ReCL00SE <95970740+ReCL00SE@users.noreply.github.com>
2022-10-02 22:49:34 +0500

    update 1.1

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,14 @@
 $('.my_fade').slick({
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    prevArrow: $('.slick-arrow.left'),
-    nextArrow: $('.slick-arrow.right'),
-  });
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  prevArrow: $('.slick-arrow.left'),
+  nextArrow: $('.slick-arrow.right'),
+});
+
+$(document).ready(function () {
+  $('input[data-plugin=\'phone-mask\']').inputmask({
+    'mask': '+\\9\\9\\8 (99) 999-99-99'
+  })
+});
